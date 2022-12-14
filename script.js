d40bb82fc9f0c4d515d6944e7a34916beee28c7a
GuildOfNewton <120147872+GuildOfNewton@users.noreply.github.com>
2022-12-13 22:17:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function loadFrame(url, name) {
   var div = document.getElementById("calendar_div");
   var placeholder = document.getElementById("cal_loading");
   var frame = document.createElement("iframe");
-  fetch(url).then((response) -> response.json()).then((data) => frame.srcdoc = data);
+  fetch(url).then((response) => response.json()).then((data) => frame.srcdoc = data);
   //frame.src = url;
   frame.classList.add("calendar");
   frame.style.visibility = "visible";
